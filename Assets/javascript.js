21324c2744251d773fd02aa9de07f9e3b718bb2b
Carolina Reedy <carinvid@gmail.com>
2021-05-19 13:01:46 -0600

    addign questions for quiz

---
+++ Assets/javascript.js
@@ -0,0 +1,57 @@
+const questions = [
+  {
+    question: "What is Hoisting in javascript?",
+    answers: {
+      a: "Javascript doesn't response",
+      b: "Unknow behavior",
+      c: "it is a default bahavior of Javascript where all the variables and function declarations are moved on top.",
+      d: "There are codes that are missing",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    answers: {
+      a: "Angular",
+      b: "jQuery",
+      c: "RequireJS",
+      d: "ESLint",
+    },
+    correctAnswer: "d",
+  },
+  {
+    question: "which symbol is used for comments in javascript?",
+    answers: {
+      a: "</",
+      b: "//",
+      c: "/*",
+      d: " b and c",
+    },
+    correctAnswer: "d",
+  },
+  {
+    question: "What is === operator?",
+    answers: {
+      a: "a strict equaly operator",
+      b: "it is an error",
+      c: "not aplicable ",
+      d: "none",
+    },
+    correctAnswer: "a",
+  },
+  {
+    question: "What are JavaScript Data Types?",
+    answers: {
+      a: "Number",
+      b: "String",
+      c: "Boolean",
+      d: "All above",
+    },
+    correctAnswer: "d",
+  },
+];
+
+const score = 0;
+ for ( var i = 0; i < questions.length; i ++){
+     var response = 
+ }
